cc69871ed13d09ce6c5caa8a8c75e08ffba21923
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 03:44:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+var empty = [] ;
+function theBeatlesPlay(guys, tools){
+  for(var i = 0 ; i < guys.length ; i++){
+    var sent = `${guys[i]} plays ${tools[i]}` ;
+    empty.push(sent) ;
+  }
+}
