120caae275bb514c6c7aa3937681ced2e9df98da
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 03:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var empty = [] ;
+
 function theBeatlesPlay(guys, tools){
+  var empty = [] ;
   for(var i = 0 ; i < guys.length ; i++){
     var sent = `${guys[i]} plays ${tools[i]}` ;
     empty.push(sent) ;
